a15bf9ebbbc32c8a883a62dc770b6c24a869723b
Jonathan Wight <jwight@mac.com>
2022-10-31 15:00:44 -0700

    WIP

--- MastodonAgain/AppSettings.swift
+++ MastodonAgain/AppSettings.swift
@@ -11,25 +11,12 @@ struct AppSettings: View {
     var body: some View {
         List {
             Form {
-//                if let application = registeredApplication {
-//                    Section("Application") {
-//                        LabeledContent("id", value: String(describing: application.id))
-//                        LabeledContent("name", value: application.name)
-//                        LabeledContent("website", value: application.website)
-//                        LabeledContent("redirectURI", value: application.redirectURI)
-//                        LabeledContent("clientID", value: application.clientID)
-//                        LabeledContent("clientSecret", value: application.clientSecret)
-//                        LabeledContent("vapidKey", value: application.vapidKey)
-//                    }
-//                }
-//                if let token = token {
-//                    Section("Token") {
-//                        LabeledContent("accessToken", value: token.accessToken)
-//                        LabeledContent("tokenType", value: token.tokenType)
-//                        LabeledContent("scope", value: token.scope)
-//                        LabeledContent("created", value: "\(token.created, format: .dateTime)")
-//                    }
-//                }
+
+
+
+                DebugDescriptionView(appModel.authorization)
+
+
                 Button("Log out") {
                     appModel.authorization = .unauthorized
                 }
