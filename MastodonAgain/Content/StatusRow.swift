a9a0880f68e1059c12e4479477b59f72e43a2872
Jonathan Wight <jwight@mac.com>
2022-11-20 10:50:51 -0800

    Minor re-org.

--- MastodonAgain/Content/StatusRow.swift
+++ MastodonAgain/Content/StatusRow.swift
@@ -21,3 +21,85 @@ struct StatusRow: View {
         }
     }
 }
+
+// MARK: -
+
+struct SensitiveContentModifier: ViewModifier {
+    let sensitive: Bool
+
+    func body(content: Content) -> some View {
+        if sensitive {
+            content
+                .blur(radius: 20)
+                .clipped()
+                .cornerRadius(8)
+                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.red))
+        }
+        else {
+            content
+        }
+    }
+}
+
+extension View {
+    func sensitiveContent(_ sensitive: Bool) -> some View {
+        modifier(SensitiveContentModifier(sensitive: sensitive))
+    }
+}
+
+// MARK: -
+
+struct StatusContent<Status>: View where Status: StatusProtocol {
+    @EnvironmentObject
+    var appModel: AppModel
+
+    let status: Status
+
+    var sensitive: Bool {
+        status.sensitive
+    }
+
+    var hideContent: Bool {
+        sensitive && !allowSensitive && appModel.hideSensitiveContent
+    }
+
+    @State
+    var allowSensitive = false
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            if sensitive && appModel.hideSensitiveContent == true {
+                HStack() {
+                    status.spoilerText.nilify().map(Text.init)
+                    Toggle("Show Sensitive Content", isOn: $allowSensitive)
+                }
+                .controlSize(.small)
+            }
+            VStack(alignment: .leading) {
+                // TODO: Gross.
+                if appModel.useMarkdownContent {
+                    (try? status.markdownContent).map { Text($0).textSelection(.enabled) }
+                }
+                else {
+                    (try? status.attributedContent).map { Text($0).textSelection(.enabled) }
+                }
+                if !status.mediaAttachments.isEmpty {
+                    MediaStack(attachments: status.mediaAttachments)
+                }
+                if let poll = status.poll {
+                    Text("Poll: \(String(describing: poll))").debuggingInfo()
+                }
+                if let card = status.card {
+                    CardView(card: card)
+                }
+            }
+            .sensitiveContent(hideContent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            //            .overlay {
+            //                if sensitive && !allowSensitive {
+            //                    Color.red.opacity(1).backgroundStyle(.thickMaterial)
+            //                }
+            //            }
+        }
+    }
+}
